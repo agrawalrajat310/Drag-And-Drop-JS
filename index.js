8a8ceac04ecd382736fe7142c7db10f4b06a3d85
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-10 16:26:11 +0530

    code updated

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // Javascript code
 
 
+let contn1 = document.getElementById("cont1");
 
+let contn2 = document.getElementById("cont2");
 
+let items = document.querySelectorAll("submain");
 
+// Event listeners :-
+
+items.forEach(val => {
+    val.addEventListener("dstart", start);
+    val.addEventListener("dend", end);
+});
 
 
 
