0a2d040badc23d8f13b04d6f2e6475a23fe2e9fd
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-06-10 16:32:00 +0530

    code updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Javascript code
 
-
 let contn1 = document.getElementById("cont1");
 
 let contn2 = document.getElementById("cont2");
@@ -9,19 +8,19 @@ let items = document.querySelectorAll("submain");
 
 // Event listeners for container 1 :-
 
-items.forEach(val => {
-    val.addEventListener("dstart", start);
-    val.addEventListener("dend", end);
+items.forEach((val) => {
+  val.addEventListener("dstart", dStart);
+  val.addEventListener("dend", dEnd);
 });
 
 // Event listeners for container 2 :-
 
 contn2.addEventListener("drop", drop);
-contn2.addEventListener("dover", over);
+contn2.addEventListener("dover", dOver);
 
+// event handler for drag start
 
-
-
-
-
-
+function dStart(e) {
+  e.dataTransfer.setData("text/plain", e.target.id);
+  e.target.classList.add("dragged");
+}
